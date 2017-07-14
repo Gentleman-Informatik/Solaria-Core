75360edea4987356f3cb3bdc3b2c229231cc5863
flaver <flaverkleiber@yahoo.de>
2017-07-14 15:50:35 +0100

    Well, dont push to much.....

---
+++ Application/Page/Controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Solaria\Application\Page\Controllers;
+
+use Solaria\Framework\Application\Mvc\BaseController;
+
+class IndexController extends BaseController {
+
+    public function indexAction() {
+        $this->view->set('hello', 'world!');
+    }
+
+}
